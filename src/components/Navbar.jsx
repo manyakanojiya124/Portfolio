d1dc94e5e6fc1f0cdcfe2c08cbe7b28a439df607
manyakanojia124 <manya04092005@gmail.com>
2026-04-24 00:29:17 +0530

    first commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -45,7 +45,7 @@ const Navbar = () => {
           <img src={logo} alt='logo' className='w-9 h-9 object-contain' />
           <p className='text-white text-[18px] font-bold cursor-pointer flex '>
             Manya Kanojia &nbsp;
-            <span className='sm:block hidden'> | Cloud and AI</span>
+            <span className='sm:block hidden'> | Student</span>
           </p>
         </Link>
 
